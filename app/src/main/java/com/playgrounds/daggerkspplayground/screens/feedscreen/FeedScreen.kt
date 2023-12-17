527e88d4c3156ad2428199bea807d83e2a77b8d0
Shmuel Meymann <shmuel.m@zemingo.com>
2023-12-17 21:39:41 +0200

    Linear progress indicator

--- app/src/main/java/com/playgrounds/daggerkspplayground/screens/feedscreen/FeedScreen.kt
+++ app/src/main/java/com/playgrounds/daggerkspplayground/screens/feedscreen/FeedScreen.kt
@@ -4,17 +4,16 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
@@ -34,33 +33,29 @@ fun FeedScreen(
     onItemClicked: (FeedRepo.RssItem) -> Unit,
 ) {
     CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Rtl) {
-        Box(
-            modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ) {
-            Column(modifier = Modifier.fillMaxSize()) {
-                TabRow(selectedTabIndex = selectedTabIndex) {
-                    tabs.forEach { tab ->
-                        Tab(
-                            selected = false,
-                            onClick = { onTabSelected(tab) },
-                            text = { Text(text = stringResource(id = tab.nameRes)) }
-                        )
-                    }
+        Column(modifier = Modifier.fillMaxSize()) {
+            TabRow(selectedTabIndex = selectedTabIndex) {
+                tabs.forEach { tab ->
+                    Tab(
+                        selected = false,
+                        onClick = { onTabSelected(tab) },
+                        text = { Text(text = stringResource(id = tab.nameRes)) }
+                    )
                 }
+            }
+            Box(modifier = Modifier.fillMaxSize()) {
                 LazyColumn(Modifier.padding(16.dp)) {
                     items(feedData.size) { i ->
                         FeedCell(feedData[i], onItemClicked)
                     }
                 }
-            }
-
-            if (isLoading) {
-                CircularProgressIndicator(
-                    modifier = Modifier.width(64.dp),
-                    color = MaterialTheme.colorScheme.secondary,
-                    trackColor = MaterialTheme.colorScheme.surfaceVariant,
-                )
+                if (isLoading) {
+                    LinearProgressIndicator(
+                        modifier = Modifier.fillMaxWidth(),
+                        color = MaterialTheme.colorScheme.secondary,
+                        trackColor = MaterialTheme.colorScheme.surfaceVariant,
+                    )
+                }
             }
         }
     }
