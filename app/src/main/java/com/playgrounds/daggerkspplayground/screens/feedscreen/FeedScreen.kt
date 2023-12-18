47db117231c74c1ecb616f7229331297f3d7d04f
Shmuel Meymann <shmuel.m@zemingo.com>
2023-12-18 11:45:41 +0200

    Preview to tabs

--- app/src/main/java/com/playgrounds/daggerkspplayground/screens/feedscreen/FeedScreen.kt
+++ app/src/main/java/com/playgrounds/daggerkspplayground/screens/feedscreen/FeedScreen.kt
@@ -19,8 +19,11 @@ import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextDirection
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
+import androidx.core.text.toSpannable
+import com.playgrounds.daggerkspplayground.R
 import com.playgrounds.daggerkspplayground.repos.FeedRepo
 
 @Composable
@@ -69,4 +72,21 @@ fun FeedCell(rssFeed: FeedRepo.RssItem, onItemClicked: (FeedRepo.RssItem) -> Uni
         Text(text = rssFeed.title, style = TextStyle(textDirection = TextDirection.Content))
         Text(text = rssFeed.description.toString(), style = TextStyle(textDirection = TextDirection.Content))
     }
+}
+
+
+@Preview(backgroundColor = 0xFFFFFFFF, showBackground = true)
+@Composable
+fun FeedScreenPreview() {
+    FeedScreen(
+        isLoading = false,
+        tabs = listOf(TabDescriptor("Tab 1", R.string.sports), TabDescriptor("Tab 2", R.string.news)),
+        feedData = listOf(
+            FeedRepo.RssItem("Title 1", "Link 1", "Description 1".toSpannable()),
+            FeedRepo.RssItem("Title 2", "Link 2", "Description 2".toSpannable()),
+            FeedRepo.RssItem("Title 3", "Link 3", "Description 3".toSpannable()),
+        ),
+        onTabSelected = {},
+        onItemClicked = {},
+    )
 }
